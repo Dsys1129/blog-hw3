bef84410d54b9dcfcd9c075c629e065517881ef4
Dsys1129 <dsys1129@gmail.com>
2023-07-03 21:17:45 +0900

    feat : User Entity UserRole 필드 추가

--- src/main/java/com/example/bloghw3/user/entity/User.java
+++ src/main/java/com/example/bloghw3/user/entity/User.java
@@ -2,6 +2,8 @@ package com.example.bloghw3.user.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -27,9 +29,13 @@ public class User {
     @Column(nullable = false)
     private String password;
 
+    @Enumerated(value = EnumType.STRING)
+    private UserRole userRole;
+
     @Builder
     private User(String username, String password) {
         this.username = username;
         this.password = password;
+        this.userRole = UserRole.USER;
     }
 }
